a9e0bdfa0f787960f44b36a8f519f9e3f911dd14
Nathan <drugen@gmail.com>
2022-05-29 16:39:45 -0600

    working on connecting automatically!

--- django_glue/templatetags/django_glue.py
+++ django_glue/templatetags/django_glue.py
@@ -1,6 +1,7 @@
 import logging
 
 from django import template
+from django.utils.safestring import mark_safe
 
 from django_glue.utils import convert_glue_dict_to_json
 
@@ -11,3 +12,20 @@ register = template.Library()
 def glue_js(context):
     context['glue_json_string'] = convert_glue_dict_to_json(context['glue'])
     return context
+
+
+@register.simple_tag(takes_context=True, name='glue_connect')
+def glue_connect(context, target):
+    target_key_list = target.split('.')
+    value = ''
+    if len(target_key_list) == 3:
+        if target_key_list[0] in context['glue']:
+            if target_key_list[1] in context['glue'][target_key_list[0]]:
+                if target_key_list[0] == 'fields':
+                    if target_key_list[2] in context['glue'][target_key_list[0]][target_key_list[1]]:
+                        value = context['glue'][target_key_list[0]][target_key_list[1]][target_key_list[2]]
+                elif target_key_list[0] == 'objects':
+                    value = context['glue'][target_key_list[0]][target_key_list[1]]['data'].__dict__[target_key_list[2]]
+        return mark_safe(f'id="{target_key_list[2]}" name="{target_key_list[2]}"')
+    else:
+        return mark_safe(f'')
--- django_glue/views.py
+++ django_glue/views.py
@@ -1,6 +1,18 @@
+import json
 import logging
+
+from django.http import JsonResponse
 
 
 def glue_ajax_handler_view(request):
     logging.warning('Glue Handler Start')
-    logging.warning(f'{request.GET}')
+
+    body_data = json.loads(request.body.decode('utf-8'))
+
+    logging.warning(f'{body_data = }')
+
+    json_response = {
+        'status': True,
+    }
+
+    return JsonResponse(json_response)
