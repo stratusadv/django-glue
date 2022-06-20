56967ec2caa0724316d70f1ae2e692c7301ebfdd
Nathan Johnson <nathanj@stratusadv.com>
2022-06-20 14:32:32 -0600

    Pickled some query_sets

--- django_glue/utils.py
+++ django_glue/utils.py
@@ -1,8 +1,10 @@
-import logging, json
+import logging, json, pickle, base64
 from uuid import uuid4
 
 from django.contrib.contenttypes.models import ContentType
 from django.http import JsonResponse
+
+GLUE_SESSION_NAME = 'django_glue'
 
 GLUE_CONNECT_INPUT_METHODS = (
     'live',
@@ -34,10 +36,7 @@ GLUE_RESPONSE_TYPES = (
 
 
 def add_model_object_glue(request, unique_name, model_object, method, fields=None, **kwargs):
-    if 'django_glue' not in request.session:
-        request.session['django_glue'] = dict()
-
-    rs = request.session['django_glue']
+    rs = get_glue_session(request)
 
     content_type = ContentType.objects.get_for_model(model_object)
 
@@ -64,8 +63,35 @@ def add_model_object_glue(request, unique_name, model_object, method, fields=Non
         raise TypeError('field argument must be a str object')
 
 
-def add_model_query_set_glue(request, unique_name, model_object, filter):
-    pass
+def add_model_query_set_glue(request, unique_name, model_object, model_query_set, method):
+    import pickle
+    import base64
+
+    rs = get_glue_session(request)
+
+    content_type = ContentType.objects.get_for_model(model_object)
+
+    query_set = model_query_set.only('id')
+
+    logging.warning(pickle.dumps(query_set))
+
+    encoded_query_set = encode_query_set_to_str(query_set)
+    logging.warning(encoded_query_set)
+
+    decoded_query_set = decode_query_set_from_str(encoded_query_set)
+    logging.warning(decode_query_set_from_str(encoded_query_set))
+
+    for thing in decoded_query_set:
+        logging.warning(thing.id)
+
+    rs[unique_name] = {
+        'method': method,
+        'model': 'test_model',
+        'content_app_label': content_type.app_label,
+        'content_model': content_type.model,
+        'object_id': model_object.pk,
+        'query_set': encode_query_set_to_str(model_query_set),
+    }
 
 
 def camel_to_snake(string):
@@ -73,7 +99,22 @@ def camel_to_snake(string):
 
 
 def clean_glue_session(request):
-    request.session['django_glue'] = dict()
+    request.session[GLUE_SESSION_NAME] = dict()
+
+
+def decode_query_set_from_str(query_set_string):
+    return pickle.loads(base64.b64decode(query_set_string))
+
+
+def encode_query_set_to_str(query_set):
+    return base64.b64encode(pickle.dumps(query_set)).decode()
+
+
+def get_glue_session(request):
+    if GLUE_SESSION_NAME not in request.session:
+        request.session[GLUE_SESSION_NAME] = dict()
+
+    return request.session[GLUE_SESSION_NAME]
 
 
 def generate_field_dict(model_field, model_object):
--- tests/views.py
+++ tests/views.py
@@ -3,7 +3,7 @@ import logging
 from django.views.generic import TemplateView
 
 from tests.models import TestModel
-from django_glue.utils import add_model_object_glue, clean_glue_session
+from django_glue.utils import add_model_object_glue, add_model_query_set_glue
 
 
 class TestView(TemplateView):
@@ -12,9 +12,8 @@ class TestView(TemplateView):
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
 
-        TestModel.objects.all().delete()
-
-        logging.warning('Purged database of testing objects.')
+        # TestModel.objects.all().delete()
+        # logging.warning('Purged database of testing objects.')
 
         test_model_object = TestModel.objects.create(
             char='Some Characters',
@@ -24,10 +23,12 @@ class TestView(TemplateView):
         )
 
         # test_model_object = TestModel.objects.all().latest('id')
-
         logging.warning(f'Added TestModel object.')
 
         add_model_object_glue(self.request, 'test_model', test_model_object, 'change')
-        logging.warning('Added model field glue for TestModel Object in write mode')
+        logging.warning('Added model object glue for TestModel Object in write mode')
+
+        add_model_query_set_glue(self.request, 'test_model_set', test_model_object, TestModel.objects.all(), 'read')
+        logging.warning('Added model query set glue for TestModel Object in read mode')
 
         return context_data
