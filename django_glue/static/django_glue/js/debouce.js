50827d1f47d5089d1692dac718de8606c903dd31
Nathan <drugen@gmail.com>
2022-08-02 20:53:02 -0600

    Clean out old files

--- django_glue/static/django_glue/js/debouce.js
+++
@@ -1,8 +0,0 @@
-let debounce_method = null
-
-function debounce(method, interval = 300) {
-    clearTimeout(debounce_method);
-    debounce_method = setTimeout(method, interval)
-}
-
-export { debounce }
