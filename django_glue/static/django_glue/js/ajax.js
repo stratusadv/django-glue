53afba1da3d47d3ca834a8d9e23cd26570d0dc0d
Nathan Johnson <nathanj@stratusadv.com>
2023-04-02 20:33:27 -0600

    Remove Axios
    removed axios and rewrote the ajax call in fetch!

--- django_glue/static/django_glue/js/ajax.js
+++ django_glue/static/django_glue/js/ajax.js
@@ -1,18 +1,24 @@
 import {csrf_token} from "./csrf.js";
 
 async function ajax_request(method, unique_name, data) {
-    return axios({
+    const requestOptions = {
         method: method,
-        url: DJANGO_GLUE_AJAX_URL,
         headers: {
             'Content-Type': 'application/json',
             'X-CSRFToken': csrf_token,
         },
-        data: {
+        body: JSON.stringify({
             'unique_name': unique_name,
             'data': data,
-        }
-    })
+        }),
+    };
+
+    const response = await fetch(DJANGO_GLUE_AJAX_URL, requestOptions);
+    if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`);
+    }
+
+    return await response.json();
 }
 
 export {ajax_request,}
