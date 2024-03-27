9acdf106d5c62149b25ff73f187af1faf6b7d567
Wesley Howery <wesleyh@stratusadv.com>
2024-03-27 13:35:43 -0600

    Glue js _meta renamed to fields

--- django_glue/static/django_glue/js/django_glue_model_object.js
+++ django_glue/static/django_glue/js/django_glue_model_object.js
@@ -2,16 +2,11 @@ class GlueModelObject {
     constructor(glue_unique_name) {
         // Needs to be named glue_unique_name to avoid overriding the unique_name property
         this.glue_unique_name = encodeUniqueName(glue_unique_name)
-        this['_meta'] = {}
+        this['fields'] = {}
         for (let key in window.glue_session_data['context'][this.glue_unique_name].fields) {
             this[key] = window.glue_session_data['context'][this.glue_unique_name].fields[key].value
-
-            this['_meta'][key] = window.glue_session_data['context'][this.glue_unique_name].fields[key]
+            this['fields'][key] = window.glue_session_data['context'][this.glue_unique_name].fields[key]
         }
-
-        console.log('Glue js')
-        console.log(window.glue_session_data['context'][this.glue_unique_name])
-
         window.glue_keep_live.add_unique_name(this.glue_unique_name)
     }
 
