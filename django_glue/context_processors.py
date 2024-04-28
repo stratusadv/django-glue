8af223955cbc9479e1c1ebad38ec7fddc14ab591
NathanJohnson <nathanj@stratusadv.com>
2024-04-28 12:57:54 -0600

    Review use of setting, Remove Dead Settings, Reconfigure Keep Live and Update Test Settings.

--- django_glue/context_processors.py
+++ django_glue/context_processors.py
@@ -10,6 +10,6 @@ def glue(request):
         'DJANGO_GLUE_VERSION': __version__,
         settings.DJANGO_GLUE_CONTEXT_NAME: request.session.get(settings.DJANGO_GLUE_SESSION_NAME, {}),
         settings.DJANGO_GLUE_KEEP_LIVE_CONTEXT_NAME: request.session.get(settings.DJANGO_GLUE_KEEP_LIVE_SESSION_NAME, {}),
-        'DJANGO_GLUE_KEEP_LIVE_INTERVAL_TIME_MILLISECONDS': settings.DJANGO_GLUE_KEEP_LIVE_INTERVAL_TIME_SECONDS * 1000,
+        'DJANGO_GLUE_KEEP_LIVE_INTERVAL_TIME_MILLISECONDS': (settings.DJANGO_GLUE_KEEP_LIVE_EXPIRE_TIME_SECONDS / 2.2) * 1000,
         'GLUE_SESSION_DATA': json.dumps(GlueSession(request).session),
     }
