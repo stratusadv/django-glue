441a82aedb251dbacdd9dbcd5a9f672c8510ba2d
Wesley Howery <wesleyh@stratusadv.com>
2024-08-10 08:59:11 -0600

    glue fetch refactored for configuration

--- django_glue/static/django_glue/js/core/ajax.js
+++ django_glue/static/django_glue/js/core/ajax.js
@@ -2,42 +2,78 @@ async function glue_ajax_request(
     unique_name,
     action,
     data = {},
-    content_type = 'application/json',
-    method = 'POST',
+    {
+        content_type = 'application/json',
+        method = 'POST',
+    } = {},
 ) {
     return await glue_fetch(DJANGO_GLUE_AJAX_URL, {
+        payload: {
         'unique_name': unique_name,
         'action': action,
         'data': data,
+        }
     })
+}
+
+
+function format_get_url(url, payload = {}) {
+    let formatted_url = new URL(url);
+    Object.entries(payload).forEach(([key, value]) => {
+        url.searchParams.append(key, value);
+    });
+    formatted_url = url.toString();
+    return formatted_url;
 }
 
 
 async function glue_fetch(
     url,
-    body = {},
-    content_type = 'application/json',
-    method = 'POST',
+    {
+        payload = {},
+        method = 'POST',
+        content_type = 'application/json',
+        headers = {},
+        response_type = 'json',
+    } = {}
 ) {
+    const csrf_token = glue_get_cookie('csrftoken')
+
     const request_options = {
-        method: method,
+        method,
         headers: {
             'Content-Type': content_type,
-            'X-CSRFToken': glue_get_cookie('csrftoken'),
+            'X-CSRFToken': csrf_token,
+            ...headers,
         },
-        body: JSON.stringify(body),
+    };
+
+    if (method !== 'GET' && method !== 'HEAD') {
+        request_options.body = JSON.stringify(payload)
+    } else {
+        url = format_get_url(url, payload)
     }
 
-    const response = await fetch(url, request_options)
+    try {
+        const response = await fetch(url, request_options);
 
-    if (!response.ok) {
-        throw new Error(`HTTP error ${response.status}`)
-    }
+        if (!response.ok) {
+            const errorBody = await response.text();
+            throw new Error(`HTTP error ${response.status}: ${errorBody}`);
+        }
 
-    if (content_type === 'application/json') {
-        return response.json()
-    }
-    else {
-        return response
+        switch (response_type) {
+            case 'json':
+                return await response.json();
+            case 'text':
+                return await response.text();
+            case 'blob':
+                return await response.blob();
+            default:
+                return response;
+        }
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error;
     }
 }
