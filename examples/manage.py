f8cdd27d6e61b273eb99657c86c356d1b8cd85c7
TyrellMartens <tyrellm@stratusadv.com>
2025-03-07 16:02:50 -0700

    Updated project structure to match closer to previous glue structure.

--- examples/manage.py
+++ examples/manage.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'examples.system.development.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'examples.system.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- examples/system/testing/urls.py
+++
@@ -1,1 +0,0 @@
-from system.development.urls import *
