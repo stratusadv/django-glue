1681caefd1befdd708526e4fe91ca9bea8ffa243
Brayden Carlson <braydenc@stratusadv.com>
2025-03-28 23:01:38 -0600

    Add manage.py'

---
+++ manage.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import os
+import sys
+
+
+def main() -> None:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example.system.settings')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        message = (
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        )
+
+        raise ImportError(message) from exc
+
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
