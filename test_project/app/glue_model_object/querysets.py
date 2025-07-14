22c1e748c342da70bf273416a78223b2259d0ff4
Wesley Waldern <wesleyw@stratusadv.com>
2025-07-14 09:41:55 -0600

    getting test glue model object up and running, very basic idea

--- test_project/app/glue_model_object/querysets.py
+++ test_project/app/glue_model_object/querysets.py
@@ -2,5 +2,4 @@ from django.db import  models
 
 
 class TestGlueModelObjectQuerySet(models.QuerySet):
-    def active(self):
-        return self.filter(is_active=True, is_deleted=False)
+    pass
--- test_project/app/glue_model_object/views/page_views.py
+++ test_project/app/glue_model_object/views/page_views.py
@@ -1,11 +1,14 @@
 from django.template.response import TemplateResponse
 
 from test_project.app.glue_model_object import models
+from test_project.app.glue_model_object.utils import generate_randomized_test_model
+
 
 def list_view(request):
+    generate_randomized_test_model()
 
     context_data = {
-        'glue_model_object_list' : models.TestGlueModelObject.objects.active()
+        'glue_model_object_list' : models.TestGlueModelObject.objects.all()
     }
 
     return TemplateResponse(
