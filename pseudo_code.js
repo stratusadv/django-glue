bc4fd6c0a72af0a19ec399f67088a26229fbec6e
Nathan Johnson <nathanj@stratusadv.com>
2026-02-05 15:32:44 -0700

    initial

--- pseudo_code.js
+++ pseudo_code.js
@@ -1,1 +1,14 @@
-dg.<unique_name>.first_name
+// loaded automatically and is constructed from the context data?
+
+window.document.dg = DjangoGlueInit()
+
+let Glue = window.document.dg
+
+// using in Javascript File
+
+let first_name = Glue.the_unique_name.first_name
+// Lazy loads the object on the first_name attribute access
+
+// Using in a function
+
+
