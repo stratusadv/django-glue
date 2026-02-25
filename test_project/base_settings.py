548d82ce4384d5cbe6784382f26d35881e7dcb4e
Nathan Johnson <nathanj@stratusadv.com>
2026-02-25 09:11:36 -0700

    update settings

--- test_project/base_settings.py
+++ test_project/base_settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'django_glue_test_project.db',
     }
 }
 
