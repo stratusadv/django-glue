dedcb3c06803704f7fe304de825a30d0056334e8
Nathan Johnson <nathanj@stratusadv.com>
2026-03-31 15:40:14 -0600

    initial fix

--- django_glue/static/django_glue/js/session/keep_live.js
+++ django_glue/static/django_glue/js/session/keep_live.js
@@ -1,7 +1,33 @@
 class DjangoGlueKeepLive {
-    constructor() {
+    constructor(keep_live_interval_milliseconds) {
         window.django_glue_keep_live_unique_names = []
         window.django_glue_session_data = {}
+        this.last_updated_datetime_milliseconds = 0
+        this.keep_live_interval_milliseconds = keep_live_interval_milliseconds
+    }
+
+    set_last_updated_date_time() {
+        this.last_updated_datetime_milliseconds = Date.now()
+    }
+
+    check_expired() {
+        let is_expired = (Date.now() - this.last_updated_datetime_milliseconds) > (this.keep_live_interval_milliseconds + 5000)
+
+        if (is_expired) {
+            this.confirm_reload()
+        } else {
+            setTimeout(() => {
+                this.check_expired()
+            }, 3000)
+        }
+    }
+
+    confirm_reload() {
+        let confirmation = confirm('Session expired. Do you want to reload the page?')
+
+        if (confirmation) {
+            window.location.reload()
+        }
     }
 
     add_unique_name(unique_name) {
@@ -25,11 +51,9 @@ class DjangoGlueKeepLive {
         const response = await fetch(keep_live_url, request_options)
 
         if (!response.ok) {
-            let confirmation = confirm('Session expired. Do you want to reload the page?')
-
-            if (confirmation) {
-                window.location.reload()
-            }
+            this.confirm_reload()
+        } else {
+            this.set_last_updated_date_time()
         }
     }
 }
