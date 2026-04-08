a2cf040d377ca07cde814df0efef2d395c85a6ee
chasemstratusadv <chasem@stratusadv.com>
2026-04-08 10:14:28 -0600

    refactor fix

--- django_glue/static/django_glue/js/session/keep_live.js
+++ django_glue/static/django_glue/js/session/keep_live.js
@@ -2,8 +2,9 @@ class DjangoGlueKeepLive {
     constructor(keep_live_interval_milliseconds) {
         window.django_glue_keep_live_unique_names = []
         window.django_glue_session_data = {}
-        this.last_updated_datetime_milliseconds = 0
+        this.last_updated_datetime_milliseconds = Date.now()
         this.keep_live_interval_milliseconds = keep_live_interval_milliseconds
+        this.keep_live_interval_handle = null
     }
 
     set_last_updated_date_time() {
@@ -11,18 +12,19 @@ class DjangoGlueKeepLive {
     }
 
     check_expired() {
-        let is_expired = (Date.now() - this.last_updated_datetime_milliseconds) > (this.keep_live_interval_milliseconds + 5000)
+
+        const now_last_expired_delta = (Date.now() - this.last_updated_datetime_milliseconds)
+        let is_expired = now_last_expired_delta > (this.keep_live_interval_milliseconds + 5000)
+
 
         if (is_expired) {
             this.confirm_reload()
-        } else {
-            setTimeout(() => {
-                this.check_expired()
-            }, 3000)
         }
     }
 
     confirm_reload() {
+        clearInterval(this.keep_live_interval_handle)
+
         let confirmation = confirm('Session expired. Do you want to reload the page?')
 
         if (confirmation) {
@@ -37,6 +39,8 @@ class DjangoGlueKeepLive {
     }
 
     async update(keep_live_url) {
+        this.check_expired()
+
         const request_options = {
             method: 'POST',
             headers: {
@@ -55,5 +59,11 @@ class DjangoGlueKeepLive {
         } else {
             this.set_last_updated_date_time()
         }
+    }
+
+    init() {
+        this.keep_live_interval_handle = setInterval(() => {
+            window.django_glue_keep_live.update(DJANGO_GLUE_KEEP_LIVE_URL)
+        }, this.keep_live_interval_milliseconds)
     }
 }
