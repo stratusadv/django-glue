a7d0078a8e1bcaffb9819c95d5d37e91a06d761f
chasemstratusadv <chasem@stratusadv.com>
2026-07-17 14:37:23 -0600

    fix response

--- django_glue/response.py
+++ django_glue/response.py
@@ -1,21 +1,39 @@
-from dataclasses import dataclass
-from typing import Any, ClassVar, Iterable
+from dataclasses import dataclass, field
+from typing import Any, ClassVar
 
+from django.http import JsonResponse
 from django.urls import reverse
 
 from django_glue.message import GlueMessage
+from django_glue.resolver.attribute_event.encoders import BoundAttributeDataJSONEncoder
 
 
 @dataclass
 class GlueResponse:
     Message: ClassVar[type] = GlueMessage
-
+    state: dict | None = None
+    policy: dict | None = None
     result: Any = None
-    messages: Iterable[GlueMessage] | None = None
+    messages: list[GlueMessage] = field(default_factory=list)
     status: int = 200
 
-    def __post_init__(self) -> None:
-        self.messages = list(self.messages or [])
+    def to_json_response(self) -> JsonResponse:
+        data = {
+            'result': self.result if self.result is not None else {},
+            'state': self.state,
+            'messages': [message.to_dict() for message in self.messages]
+        }
+        if self.policy is not None:
+            data['policy'] = self.policy
+        if self.messages is not None:
+            data['messages'] = [message.to_dict() for message in self.messages]
+
+        return JsonResponse(
+            data=data,
+            status=self.status,
+            safe=True,
+            encoder=BoundAttributeDataJSONEncoder,
+        )
 
 
 class GlueRedirectResponse:
